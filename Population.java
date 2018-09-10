8e38cbf84e13e5f6f7fb8df9036c1e07609a811f
joseph <josephgk@hotmail.nl>
2018-09-10 16:43:16 +0200

    init

---
+++ Population.java
@@ -0,0 +1,134 @@
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * Created by Joseph on 9/8/2018.
+ */
+
+public class Population {
+    private Random _rnd;
+    private ArrayList<Child> children = new ArrayList<>();
+    int TIME = 100;
+
+    public Population(Random rnd) {
+        _rnd = rnd;
+        children.add(new Child(_rnd));
+    }
+
+    public Child[] SelectMaxParents(int amount_of_parents) {
+        if (children.size() < 2) {
+            return new Child[]{children.get(0)};
+        }
+        Child[] maxparents = new Child[amount_of_parents];
+        for (int i = 0; i < amount_of_parents; i++) maxparents[i] = children.get(i);
+        return maxparents;
+    }
+
+    public Child[] SelectBoltzmanParents(int amount_of_parents, int eval) {
+        //https://www.rug.nl/research/portal/files/61756372/ICAART_2018_27.pdf
+        if (children.size() < 2) {
+            return new Child[]{children.get(0)};
+        }
+
+        double T = Math.max(0.5,TIME /(double) eval);
+        double[] probabilities = new double[children.size()];
+
+        double denom = 0;
+        for (Child c : children)
+        {
+            denom += Math.exp(c.getFitness() / T);
+        }
+
+        for (int i = 0; i < children.size(); i++) {
+            Child child = children.get(i);
+            //devide by 10 to avoid scaling issues
+            double numerator = Math.exp((child.getFitness()) / T);
+            probabilities[i] = numerator / denom;
+        }
+        //System.out.println(Arrays.toString(probabilities));
+        double cumsum = 0;
+        double[] cumsumprobability = new double[children.size()];
+        for (int idx = 0; idx < children.size(); idx++) {
+            cumsum += probabilities[idx] * 100;
+            cumsumprobability[idx] = cumsum;
+        }
+
+        Child[] parents = new Child[amount_of_parents];
+
+        for (int i = 0; i < amount_of_parents; i++) {
+            double randomValue = _rnd.nextDouble() * 100;
+            get_child:
+            {
+                for (int idx = 0; idx < children.size(); idx++) {
+                    if (randomValue < cumsumprobability[idx]) {
+                        parents[i] = children.get(idx);
+                        break get_child;
+                    }
+                }
+                parents[i] = children.get(0);
+            }
+
+        }
+        if(parents[0]==null || parents[1]==null){
+            System.out.println();
+        }
+        return parents;
+
+    }
+
+
+    public Child UniformCrossover(Child[] parents) {
+
+        //select random crossover points from all parents
+        int parentsize = parents.length;
+        //we apply random crossover now
+        double[] vals = new double[Child.VALUES_SIZE];
+        for (int i = 0; i < Child.VALUES_SIZE; i++) {
+            vals[i] = parents[_rnd.nextInt(parentsize)].getValues(i);
+        }
+        return new Child(vals, _rnd);
+
+    }
+
+    public Child SimpleRandomAdditionMutation(Child child) {
+        //adds random mutation values at random locations
+
+        _rnd.nextDouble();
+        double[] vals = new double[Child.VALUES_SIZE];
+        for (int i = 0; i < Child.VALUES_SIZE; i++) {
+            if (_rnd.nextDouble() > 0.8) {
+                double val = child.getValues(i) + child.random_bounded_value();
+                double rebound = child.rebound(val);
+                vals[i] = rebound;
+            } else {
+                vals[i] = child.getValues(i);
+            }
+        }
+        return new Child(vals, _rnd);
+    }
+
+
+    public void AddChild(Child child) {
+
+        int left, right, mid;
+        left = 0;
+        right = children.size();
+
+        //mergesort
+        while (left < right) {
+            mid = (left + right) / 2;
+            double result = child.getFitness() - children.get(mid).getFitness();
+            if (result > 0) { //If e is lower
+                right = mid;
+            } else { //If e is higher
+                left = mid + 1;
+            }
+        }
+        if (children.size() < 100) children.add(left, child);
+        else if (left < 100) children.set(left, child);//Drop everything after 1k
+    }
+}
+
---
+++ player24.java
@@ -0,0 +1,81 @@
+import org.vu.contest.ContestSubmission;
+import org.vu.contest.ContestEvaluation;
+
+import javax.swing.plaf.synth.SynthTextAreaUI;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Properties;
+
+public class player24 implements ContestSubmission {
+    Random rnd_;
+    ContestEvaluation evaluation_;
+    private int evaluations_limit_;
+
+    public player24() {
+        rnd_ = new Random();
+    }
+
+    public void setSeed(long seed) {
+        // Set seed of algortihms random process
+        rnd_.setSeed(seed);
+    }
+
+    public static void main(String[] args) {
+        Population pop = new Population(new Random());
+        Child child = new Child(new Random());
+        System.out.println("SchaffersEvaluation");ConsertTestBox.main(new String[]{"-submission=player24", "-evaluation=SchaffersEvaluation", "-seed=1"});
+        //System.out.println("KatsuuraEvaluation");ConsertTestBox.main(new String[]{"-submission=player24", "-evaluation=KatsuuraEvaluation", "-seed=1"});
+        //System.out.println("BentCigarFunction");ConsertTestBox.main(new String[]{"-submission=player24", "-evaluation=BentCigarFunction", "-seed=1"});
+
+    }
+
+    public void setEvaluation(ContestEvaluation evaluation) {
+        // Set evaluation problem used in the run
+        evaluation_ = evaluation;
+
+
+        // Get evaluation properties
+        Properties props = evaluation.getProperties();
+        // Get evaluation limit
+        evaluations_limit_ = Integer.parseInt(props.getProperty("Evaluations"));
+        // Property keys depend on specific evaluation
+        // E.g. double param = Double.parseDouble(props.getProperty("property_name"));
+        boolean isMultimodal = Boolean.parseBoolean(props.getProperty("Multimodal"));
+        boolean hasStructure = Boolean.parseBoolean(props.getProperty("Regular"));
+        boolean isSeparable = Boolean.parseBoolean(props.getProperty("Separable"));
+
+        // Do sth with property values, e.g. specify relevant settings of your algorithm
+        if (isMultimodal) {
+            // Do sth
+        } else {
+            // Do sth else
+        }
+    }
+
+    public void run() {
+        // Run your algorithm here
+        int evals = 0;
+        // init population
+        // calculate fitness
+        System.out.println("test");
+        Population pop = new Population(rnd_);
+        while (evals < evaluations_limit_) {
+            Child[] parents = pop.SelectBoltzmanParents(2,evals);
+            //creating the child
+            Child child = pop.UniformCrossover(parents);
+            child = pop.SimpleRandomAdditionMutation(child);
+
+            //calculating fitness
+            Double fitness = (double) evaluation_.evaluate(child.getValues());
+            child.setFitness(fitness);
+            //System.out.println(child.getFitness());
+            //System.out.println(Arrays.toString(child.getValues()));
+
+            pop.AddChild(child);
+
+            evals++;
+            // Select survivors
+        }
+
+    }
+}
