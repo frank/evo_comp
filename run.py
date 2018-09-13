4039a96131e7515e719d4d5e524a4a949e5e8202
frank <francidalca@gmail.com>
2018-09-13 13:53:29 +0200

    Fix run.py to include environment variable

--- run.py
+++ run.py
@@ -10,22 +10,35 @@ def argument_error():
     return
 
 
+def catch_error(cmd, error_msg):
+    if len(error_msg) > 2:
+        print("Issued command:")
+        print(cmd)
+        print()
+        print("Produced error:")
+        print(error_msg)
+        sys.exit()
+    return
+
+
 def get_function_name(arg):
     if arg == 'b':
         return 'BentCigarFunction'
     elif arg == 'k':
         return 'KatsuuraEvaluation'
     elif arg == 's':
-        return 'SchafferEvalution'
+        return 'SchaffersEvalution'
     else:
         argument_error()
     return None
 
 
-def run_cmd(cmd):
-    cmd = cmd.split()
+def run_cmd(cmd_ext):
+    cmd = cmd_ext.split()
     result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    return result.stdout.decode('utf-8'), result.stderr.decode('utf-8')
+    catch_error(cmd_ext, result.stderr.decode('utf-8'))
+
+    return result.stdout.decode('utf-8')
 
 
 if __name__ == '__main__':
@@ -33,16 +46,13 @@ if __name__ == '__main__':
         argument_error()
 
     function_name = get_function_name(sys.argv[1].lower())
-		
-		# ADD NEW CLASSES IN THE TWO FOLLOWING LINES
-    _, _ = run_cmd('javac -cp contest.jar player24.java Population.java Child.java')
-    _, _ = run_cmd('jar cmf MainClass.txt submission.jar player24.class Population.class Child.class')
 
-    output, error_msg = run_cmd('java -jar testrun.jar -submission=player24 -evaluation=' + function_name + ' -seed=1')
+    # ADD NEW CLASSES TO THE TWO FOLLOWING LINES
+    _ = run_cmd('javac -cp contest.jar player24.java Population.java Child.java')
+    _ = run_cmd('jar cmf MainClass.txt submission.jar player24.class Population.class Child.class')
 
-    if "javabbob" in error_msg:
-        print("Fix that javabbob error")
-        sys.exit()
+    os.putenv("LD_LIBRARY_PATH", os.getcwd())
+    output = run_cmd('java -jar testrun.jar -submission=player24 -evaluation=' + function_name + ' -seed=1')
 
     score = []
     runtime = []
@@ -57,8 +67,7 @@ if __name__ == '__main__':
 
     for i in range(n_tests):
         print(i + 1, "/", n_tests, end='\r')
-        output, error_msg = run_cmd('java -jar testrun.jar -submission=player24 -evaluation=' + function_name + ' -seed=1')
-        # print(error_msg)
+        output = run_cmd('java -jar testrun.jar -submission=player24 -evaluation=' + function_name + ' -seed=1')
         output = output.split('\n')
         for line in output:
             if "Score" in line:
@@ -67,6 +76,6 @@ if __name__ == '__main__':
             elif "Runtime" in line:
                 current_runtime = int(re.findall(r'\d+', line)[0])
                 runtime.append(current_runtime)
-		
+
     print("Average score:", sum(score) / len(score))
     print("Average runtime:", str(sum(runtime) / float(len(runtime))) + "ms")
