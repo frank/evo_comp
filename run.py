97101b7b864e5bf49332033c1908e7932f28a96c
NILOIDE <nilstoltanso@gmail.com>
2018-10-10 16:42:01 +0200

    Improved param tunning

--- run.py
+++ run.py
@@ -49,7 +49,7 @@ def getTuningParams():
             fileName = row
             continue
         items = row.split(" ")
-        print(items)
+        print("Input: " + str(items))
         if len(items) != 4:
             print("parameterTuning.txt file is written very poorly, ma boi!")
             quit()
@@ -59,7 +59,8 @@ def getTuningParams():
             values.append(a)
             a += float(items[3])
         parameters.append([items[0], values])
-    print(parameters)
+    print("Values: " + str(parameters[0]))
+    print("Values: " + str(parameters[1]))
     return parameters, fileName
 
 
@@ -69,6 +70,33 @@ def run_cmd(cmd_ext):
     catch_error(cmd_ext, result.stderr.decode('utf-8'))
     return result.stdout.decode('utf-8')
 
+
+def replaceValue(chars, fileName, n, value, varName):
+    found = False
+    for idx in range(n, len(chars) - n):
+        string = ""
+        phrase = "".join([string + str(chars[c]) for c in range(idx, idx + n)])
+        if varName + " = " == phrase:
+            found = True
+            decimal = False
+            hashlist = list(chars)
+            i = n
+            while (hashlist[idx + i] != ";"):
+                if hashlist[idx + i] == ".":  # Check if there is a decimal in the original
+                    decimal = True
+                hashlist[idx + i] = ""
+                i += 1
+            if decimal:
+                hashlist.insert(idx + n, str(value))
+            else:
+                hashlist.insert(idx + n, str(int(value)))
+            chars = "".join(hashlist)
+            break
+    if not found:
+        print("Param was not found: ", varName)
+        quit()
+    with open(fileName, "w") as f:
+        f.write(chars)
 
 def run_java(argv):
     function_name = get_function_name(argv[1].lower())
@@ -113,52 +141,52 @@ if __name__ == '__main__':
         argument_error()
 
     if len(sys.argv) == 5:
+        # To activate parameter tunning, add 1 more variable to run.py call (can be anything) eg. "python3 run.py k 5 0 a"
+        # parameterTuning.txt file structure -> line1: Name of file (including .java)
+        #                                       line2: Name of Parameter as written in the .java file (case sensitive),
+        #                                              lower bound, upper bound, interval (value type is determined by value type in .java file)
+        #                                              Separate the input with " " character (empty space)
+        #                                       line3: Same as line 2
+        # Stopping script early will result in the .java file not being reverted to original file (values will not be reverted to original)
         parameters, fileName = getTuningParams()
-
+        with open(fileName, "r") as f:
+            og_text = f.read()
         scores = []
-        firstItem = parameters[0]
-        for value in firstItem[1]:
-            with open(fileName, "r") as f:
-                chars = f.read()
-                n = len(firstItem[0] + " = ")
-                for idx in range(n, len(chars) - n):
-                    string = ""
-                    phrase = "".join([string + str(chars[c]) for c in range(idx, idx + n)])
-                    if firstItem[0] + " = " == phrase:
-                        decimal = False
-                        hashlist = list(chars)
-                        i = n
-                        while(hashlist[idx + i] != ";"):
-                            if hashlist[idx+i] == ".": #Check if there is a decimal in the original
-                                decimal = True
-                            hashlist[idx+i] =  ""
-                            i+=1
-                        if decimal:
-                            hashlist.insert(idx + n, str(value))
-                        else:
-                            hashlist.insert(idx + n, str(int(value)))
-                        chars = "".join(hashlist)
-                        break
-            with open(fileName, "w") as f:
-                f.write(chars)
-                print("\nValue: " + str(value))
+        print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+        for value2 in parameters[1][1]:
+            for value1 in parameters[0][1]:
+                with open(fileName, "r") as f:
+                    chars = f.read()
+                    n1 = len(parameters[0][0] + " = ")
+                    # Find value1
+                    replaceValue(chars, fileName, n1, value1, parameters[0][0])
+                    n2 = len(parameters[1][0] + " = ")
+                    replaceValue(chars, fileName, n2, value2, parameters[1][0])
+
+                with open(fileName, "w") as f:
+                    f.write(chars)
+                print("\n" + str(parameters[0][0]) + " value: " + str(value1))
+                print(str(parameters[1][0]) + " value: " + str(value2))
+                print("------------")
                 startTime = time.time()
                 scores.append([run_java(sys.argv)])
-                print("------------")
                 print("Predicted total duration: ",
-                      str((time.time() - startTime) * len(parameters[0][1])) + "s",
-                      "(" + str((time.time() - startTime) * len(parameters[0][1]) // 60) + "min)")
+                      str((time.time() - startTime) * len(parameters[0][1])*len(parameters[1][1])) + "s",
+                      "(" + str((time.time() - startTime) * len(parameters[0][1]*len(parameters[1][1])) // 60) + "min)")
                 print("------------")
+            print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
 
+        with open(fileName, "w") as f:
+            f.write(og_text)
         maxIdx = numpy.argmax(scores)
         print(scores)
         print(maxIdx)
-        print("Max score (" + str(scores[maxIdx]) + ") in paramerter " + firstItem[0] + " achieved by value: " + str(firstItem[1][maxIdx]))
+        print("Max score (" + str(scores[maxIdx]) + ") in paramerter " + parameters[0][0] + " achieved by value: " + str(parameters[0][1][maxIdx]))
         funcName = function_name = get_function_name(sys.argv[1].lower())
         fig, ax = plt.gcf(), plt.gca()
-        plt.plot(firstItem[1], scores)
-        plt.title(firstItem[0] + " on " + funcName + "\n(Repeated " + str(sys.argv[2]) + "times. Range: " + str(firstItem[1][0]) + " - " + str(firstItem[1][-1]) + ")")
-        fig.savefig(firstItem[0] +"_" + funcName + ".pdf")
+        plt.plot(parameters[0][1], scores)
+        plt.title(parameters[0][0] + " on " + funcName + "\n(Repeated " + str(sys.argv[2]) + "times. Range: " + str(parameters[0][1][0]) + " - " + str(parameters[0][1][-1]) + ")")
+        fig.savefig(parameters[0][0] +"_" + funcName + ".pdf")
         plt.close()
 
     else:
